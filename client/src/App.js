c6c223bf524f139f64d4502bb688f5efd51c7607
LagAnton90 <lagerlof90@gmail.com>
2018-04-24 19:14:33 +0200

    Changes to multiple components
    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,10 @@ import React, { Component } from 'react';
 import Dashboard from "./components/dashboard";
 import Widget from "./components/widget";
 import ValueDisplay from "./components/valuedisplay";
-import Cell from "./components/cell/cell"
-
-
-
+import Cell from './components/cell/cell';
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
+
 
 const styles = {
 
@@ -17,21 +15,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Dashboard>
-                   
-                    <Cell></Cell>
-                    <Widget title={"Widget 1"}>
-                        <ValueDisplay title={"Dynamic value"} api={"/api/test"} attribute={"value"}/>
-                    </Widget>
-
-                    <Widget title={"Widget 2"}>
-                        <ValueDisplay title={"Dynamic value"} api={"/api/test"} attribute={"anotherValue"}/>
-                    </Widget>
-
-                    <Widget title={"Widget 3"}>
-                        <ValueDisplay title={"Static value"} number={8}/>
-                    </Widget>
-                </Dashboard>
+               <Cell></Cell>
             </div>
         );
     }
