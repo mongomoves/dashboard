dce127f1bafa07d9023b206b009dbd9a68c84bfc
LagAnton90 <lagerlof90@gmail.com>
2018-04-24 10:27:35 +0200

    structure of cell.js
    created basic structure of cell class

---
+++ client/src/components/cell/cell.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class Cell extends Component {
+
+
+
+
+
+
+
+    render() {
+        return (
+
+            <div></div>
+        )
+
+    }
+
+
+
+
+}
+
+
+export default Cell;
+
