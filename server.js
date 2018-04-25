c095b6f47a97c8f0f5e26fe191c630ad4cc498e3
Martin Winqvist <winqvist.martin@gmail.com>
2018-04-25 16:34:31 +0200

    added error handling, new dependancy Morgan for logging

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require("morgan");
 
 const widgetsRouter = require('./routes/widgets');
 const dashboardsRouter = require('./routes/dashboards');
 
 const app = express();
 
+app.use(morgan('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/widgets', widgetsRouter);
 app.use('/api/dashboards', dashboardsRouter);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 app.use(function(req, res) {
     res.status(404).json({error: "url not found", url: req.originalUrl});
