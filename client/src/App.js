81b6eb9b0d3851ea42f696632b7e3f491559b57a
ViktorOlsson <vike_olsson@hotmail.com>
2018-04-27 12:29:14 +0200

    Fixade till skapa ny widget.
    Tog bort mellan knappar, tillägg för skapare på formuläret.

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,6 @@ import { Button } from 'react-bootstrap';
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
 
-const styles = {
-
-};
 
 
 class App extends Component {
@@ -21,57 +18,54 @@ class App extends Component {
     state = {
         modal: false,
         grafana: false,
-        kibana: false
-      }
-    
-      modalCancelHandler = () => {
-        this.setState({modal: false, grafana: false, kibana: false});
-      }
-    
-      modalShowHandler = () => {
-        this.setState({modal: true});
-      }
-
-      kibanaShowHandler = () => {
-          this.setState({kibana: true, modal: false});
-      }
-
-    
-      grafanaShowHandler = () => {
-        this.setState({grafana: true, modal: false});
+        kibana: false,
+       
     }
 
-    
+    modalCancelHandler = () => {
+        this.setState({ modal: false, grafana: false, kibana: false });
+    }
+
+    modalShowHandler = () => {
+        this.setState({ modal: true });
+    }
+
+    kibanaShowHandler = () => {
+        this.setState({ kibana: true, modal: false });
+        
+    }
+
+    hideKibanaHandler = () => {
+        this.setState({hideKibana: true, modal: false});
+    }
+
+
+    grafanaShowHandler = () => {
+        this.setState({ grafana: true, modal: false });
+    }
+
+
     render() {
         return (
             <div>
                 <Dashboard>
                     <Widget title={"Widget 1"}>
-                        <ValueDisplay title={"Dynamic value"} api={"/api/test"} attribute={"value"}/>
+                        <ValueDisplay title={"Dynamic value"} api={"/api/test"} attribute={"value"} />
                     </Widget>
 
                     <Widget title={"Widget 2"}>
-                        <ValueDisplay title={"Dynamic value"} api={"/api/test"} attribute={"anotherValue"}/>
+                        <ValueDisplay title={"Dynamic value"} api={"/api/test"} attribute={"anotherValue"} />
                     </Widget>
 
                     <Widget title={"Widget 3"}>
-                        <ValueDisplay title={"Static value"} number={8}/>
+                        <ValueDisplay title={"Static value"} number={8} />
                     </Widget>
                 </Dashboard>
-            
-                <Modal show={this.state.modal} modalClosed={this.modalCancelHandler}>
-                    <Button className="chooseGraf" bsStyle="primary" onClick={this.kibanaShowHandler}>Skapa Kibana widget</Button>
-                    <Button className="chooseGraf" bsStyle="primary" onClick={this.grafanaShowHandler}>Skapa Grafana widget</Button>
-                </Modal>
-                <Button bsStyle="primary" onClick={this.modalShowHandler}>Tryck för modal!</Button>
-                
-                <Modal show={this.state.grafana} modalClosed={this.modalCancelHandler}>
-                    <CreateCell widgetType="Grafana" />
-                </Modal>
 
-                <Modal show={this.state.kibana} modalClosed={this.modalCancelHandler}>
-                            <CreateCell widgetType="Kibana" />
+                <Modal show={this.state.modal} modalClosed={this.modalCancelHandler}>
+                    <CreateCell widgetType="Kibana/Grafana" />
                 </Modal>
+                <Button bsStyle="primary" onClick={this.modalShowHandler}>Skapa ny widget</Button>
 
             </div>
         );
--- client/src/components/CreateCell/CreateCell.js
+++ client/src/components/CreateCell/CreateCell.js
@@ -18,20 +18,22 @@ class CreateCell extends Component {
             <div>
                 <h3 className="createHeader">Skapa en ny {this.props.widgetType} widget!</h3>
                 <div className="wrapper">
-                <Button onClick={this.changeColorHandler} className="buttons" bsStyle={this.state.buttonClass} >Siffra</Button>
+                {/* <Button onClick={this.changeColorHandler} className="buttons" bsStyle={this.state.buttonClass} >Siffra</Button>
                 <Button className="buttons">Text</Button>
-                <Button className="buttons">Graf</Button><Button className="buttons">Tabell</Button>
+                <Button className="buttons">Graf</Button><Button className="buttons">Tabell</Button> */}
                 </div>
                 <form>
                     <label>Titel:</label><input className="textField" type="text" name="titel" />
                     
-                    <label>Datakälla:</label><input className="textField" type="text" name="data-source" />
+                    <label>Datakälla/URL:</label><input className="textField" type="url" name="data-source" />
                     
                     <label>Beskrivning:</label><input className="textField" type="text" name="descr" />
                     
                     <label>Värde:</label><input className="textField" type="text" name="value" />
                     
                     <label>Enhet:</label><input className="textField" type="text" name="unit" />
+
+                    <label>Skapare:</label><input className="textField" type="text" name="author" />
                     
                 </form>
             </div>
