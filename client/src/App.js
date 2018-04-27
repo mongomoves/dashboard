91857ecfc24f403c4b15723ddaf373b04135dfdc
Viktor Ekström <mongomoves@gmail.com>
2018-04-27 17:52:30 +0200

    Attempts to make dashboard dynamic

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import Dashboard from "./components/dashboard";
-import Widget from "./components/widget";
-import ValueDisplay from "./components/valuedisplay";
-import Cell from './components/cell/cell';
-import 'react-grid-layout/css/styles.css'
-import 'react-resizable/css/styles.css'
+// import Widget from "./components/widget";
+// import ValueDisplay from "./components/valuedisplay";
+// import Cell from './components/cell/cell';
+// import 'react-grid-layout/css/styles.css'
+// import 'react-resizable/css/styles.css'
 
 
-const styles = {
-
-};
+//"Cells" to pass to the dashboard
+const cells = [
+    {i: 'a', x: 0, y: 0, w: 1, h: 2, title: 'Hello'},
+    {i: 'b', x: 1, y: 0, w: 3, h: 2, title: 'World'},
+    {i: 'c', x: 4, y: 0, w: 1, h: 2, title: '!!!'}
+  ];
 
 class App extends Component {
     render() {
         return (
-           <Dashboard/>
+           <Dashboard layout={cells}/>
         );
     }
 }
--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {Responsive, WidthProvider} from 'react-grid-layout';
+import _ from 'lodash';
+import Cell from './cell/cell.js';
+import {Jumbotron} from 'react-bootstrap';
 
 const ResponsiveGRL = WidthProvider(Responsive);
 
@@ -12,22 +15,56 @@ class Dashboard extends Component {
     static defaultProps = {
         className: 'layout',
         rowHeight: 30,
-        cols: 12,
+        cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }
     };
 
     constructor(props) {
         super(props);
         
         this.state = {
-            layout: props.layout
+            layout: props.layout,
         };
+
+        this.addCell = this.addCell.bind(this);
+        // this.onLayoutChange = this.onLayoutChange.bind(this);
     }
+
+    /**
+     * Builds and returns elements based on whatever is found in state.layout.
+     */
+    generateDOM() {
+        return _.map(this.state.layout, function(i) {
+            return(
+                <div key={i.i}>
+                    <Jumbotron>
+                    <h1>{i.title}</h1>
+                    </Jumbotron>
+                </div>
+            )
+        });
+    }
+
+    // onLayoutChange(layout) {
+    //     this.setState(prevstate => ({
+    //         layout: layout
+    //     }));
+    // }
+
+    addCell(newCell) {
+        this.setState(prevState => ({
+            layout: [...prevState.layout, newCell]
+        }));
+    }
+
     render() {
-        const {children} = this.props;
-        const layout = this.state.layout;
         return(
-            <ResponsiveGRL className="layout" layout={layout} breakpoints={{lg:1200}}>
-                
+            <ResponsiveGRL
+                className={this.state.className}
+                layout={this.state.layout}
+                breakpoints={{lg:1200}}
+                //onLayoutChange={this.onLayoutChange}
+            >
+                {this.generateDOM()}
             </ResponsiveGRL>
         );
     }
