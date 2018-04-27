3ec7b6a080cd9b3aed0a33371b0328d69be206bd
ViktorOlsson <vike_olsson@hotmail.com>
2018-04-27 14:38:05 +0200

    La till placeholders på input.

--- client/src/components/CreateCell/CreateCell.js
+++ client/src/components/CreateCell/CreateCell.js
@@ -23,19 +23,21 @@ class CreateCell extends Component {
                 <Button className="buttons">Graf</Button><Button className="buttons">Tabell</Button> */}
                 </div>
                 <form>
-                    <label>Titel:</label><input className="textField" type="text" name="titel" />
+                    <label>Titel:</label><input className="textField" type="text" name="titel" placeholder="Ange titel på widget..." />
                     
-                    <label>Datakälla/URL:</label><input className="textField" type="url" name="data-source" />
+                    <label>Datakälla/URL:</label><input className="textField" type="url" name="data-source" placeholder="Ange datakälla/URL..." />
                     
-                    <label>Beskrivning:</label><input className="textField" type="text" name="descr" />
+                    <label>Beskrivning:</label><input className="textField" type="text" name="descr" placeholder="Ange beskrivning på widget..." />
                     
-                    <label>Värde:</label><input className="textField" type="text" name="value" />
+                    <label>Värde:</label><input className="textField" type="text" name="value" placeholder="Ange värde..." />
                     
-                    <label>Enhet:</label><input className="textField" type="text" name="unit" />
+                    <label>Enhet:</label><input className="textField" type="text" name="unit" placeholder="Ange enhet på widget..." />
 
-                    <label>Skapare:</label><input className="textField" type="text" name="author" />
+                    <label>Skapare:</label><input className="textField" type="text" name="author" placeholder="Ange Namn på Skapare..." />
 
-                    <label>Datum:</label><input className="textField" type="text" name="date-time" />
+                    <label>Datum:</label><input className="textField" type="text" name="date-time" placeholder="Ange Datum..." />
+
+                    <Button className="buttons" bsStyle={this.state.buttonClass}>Skapa Widget</Button>
                     
                 </form>
             </div>
