4c4a015b7c1f3546495179c53ee56afbdc3d9033
LagAnton90 <lagerlof90@gmail.com>
2018-04-29 14:04:52 +0200

    Changes
    Changes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Dashboard from "./components/dashboard";
-<<<<<<< HEAD
 import Widget from "./components/widget";
 import Cell from './components/cell/cell';
 import ValueDisplay from "./components/valuedisplay";
@@ -8,13 +7,6 @@ import MyFirstGrid from './components/cell/CellGrid';
 
 import 'react-grid-layout/css/styles.css'
 import 'react-resizable/css/styles.css'
-=======
-// import Widget from "./components/widget";
-// import ValueDisplay from "./components/valuedisplay";
-// import Cell from './components/cell/cell';
-// import 'react-grid-layout/css/styles.css'
-// import 'react-resizable/css/styles.css'
->>>>>>> 8f72354b9d8dba5f0c2395616de8a6df462a97a9
 
 
 //"Cells" to pass to the dashboard
@@ -27,11 +19,7 @@ const cells = [
 class App extends Component {
     render() {
         return (
-<<<<<<< HEAD
             <Cell title = "A new widget" creator = "John" dataURL = "https://snapshot.raintank.io/dashboard-solo/snapshot/y7zwi2bZ7FcoTlB93WN7yWO4aMiz3pZb?from=1493369923321&to=1493377123321&panelId=4"></Cell>
-=======
-           <Dashboard layout={cells}/>
->>>>>>> 8f72354b9d8dba5f0c2395616de8a6df462a97a9
         );
     }
 }
