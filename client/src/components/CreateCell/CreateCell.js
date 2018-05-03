1d9b216ef68312525af00f4af8d9f3c030e94927
ViktorOlsson <vike_olsson@hotmail.com>
2018-05-03 11:39:43 +0200

    Added disable function and clear state on button click.

--- client/src/components/CreateCell/CreateCell.js
+++ client/src/components/CreateCell/CreateCell.js
@@ -6,13 +6,58 @@ import './CreateCell.css';
 class CreateCell extends Component {
 
     state = {
-        buttonClass: 'primary'
+        title: '',
+        dataSource: '',
+        descr: '',
+        value: '',
+        unit: '',
+        author: '',
+        dateTime: ''
     }
 
-    changeColorHandler = () => {
-        this.setState({buttonClass: 'success'});
+    
+    // , dataSource: '', descr: '', value: '', unit: '', author: '', dateTime: ''
+
+    handleTitleChange = (e) => {
+        this.setState({title: e.target.value})
     }
 
+    handleDataSourceChange = (e) => {
+        this.setState({dataSource: e.target.value})
+    }
+
+    handleDescrChange = (e) => {
+        this.setState({descr: e.target.value})
+    }
+
+    handleValueChange = (e) => {
+        this.setState({value: e.target.value})
+    }
+
+    handleUnitChange = (e) => {
+        this.setState({unit: e.target.value})
+    }
+
+    handleAuthorChange = (e) => {
+        this.setState({author: e.target.value})
+    }
+
+    handleDateTimeChange = (e) => {
+        this.setState({dateTime: e.target.value})
+    }
+
+    handleClearState = () => {
+        this.setState({
+            title: '',
+            dataSource: '',
+            descr: '',
+            value: '',
+            unit: '',
+            author: '',
+            dateTime: ''
+        })
+    }
+ 
     render() {
         return (
             <div>
@@ -23,28 +68,39 @@ class CreateCell extends Component {
                 <Button className="buttons">Graf</Button><Button className="buttons">Tabell</Button> */}
                 </div>
                 <form>
-                    <label>Titel:</label><input className="textField" type="text" name="titel" placeholder="Ange titel på widget..." />
+                    <label>Titel:</label><input className="textField" type="text" name="title" value={this.state.title} onChange={this.handleTitleChange} placeholder="Ange titel på widget..." />
                     
-                    <label>Datakälla/URL:</label><input className="textField" type="url" name="data-source" placeholder="Ange datakälla/URL..." />
+                    <label>Datakälla/URL:</label><input className="textField" type="url" name="dataSource" value={this.state.dataSource} onChange={this.handleDataSourceChange} placeholder="Ange datakälla/URL..." />
                     
-                    <label>Beskrivning:</label><input className="textField" type="text" name="descr" placeholder="Ange beskrivning på widget..." />
+                    <label>Beskrivning:</label><input className="textField" type="text" name="descr" value={this.state.descr} onChange={this.handleDescrChange} placeholder="Ange beskrivning på widget..." />
                     
-                    <label>Värde:</label><input className="textField" type="text" name="value" placeholder="Ange värde..." />
+                    <label>Värde:</label><input className="textField" type="text" name="value" value={this.state.value} onChange={this.handleValueChange} placeholder="Ange värde..." />
                     
-                    <label>Enhet:</label><input className="textField" type="text" name="unit" placeholder="Ange enhet på widget..." />
+                    <label>Enhet:</label><input className="textField" type="text" name="unit" value={this.state.unit} onChange={this.handleUnitChange} placeholder="Ange enhet på widget..." />
 
-                    <label>Skapare:</label><input className="textField" type="text" name="author" placeholder="Ange Namn på Skapare..." />
+                    <label>Skapare:</label><input className="textField" type="text" name="author" value={this.state.author} onChange={this.handleAuthorChange} placeholder="Ange Namn på Skapare..." />
 
-                    <label>Datum:</label><input className="textField" type="text" name="date-time" placeholder="Ange Datum..." />
+                    <label>Datum:</label><input className="textField" type="text" name="dateTime" value={this.state.dateTime} onChange={this.handleDateTimeChange} placeholder="Ange Datum..." />
                     <label>Välj typ:</label>
                     <select>
+                        <option selected>Välj typ</option>
                         <option value="siffra">Siffra</option>
                         <option value="text">Text</option>
                         <option value="graf">Graf</option>
                         <option value="tabell">Tabell</option>
                     </select>
 
-                    <Button className="buttons" bsStyle={this.state.buttonClass}>Skapa Widget</Button>
+                    <Button className="buttons" 
+                        disabled={!this.state.title || 
+                            !this.state.dataSource ||
+                            !this.state.descr ||
+                            !this.state.value ||
+                            !this.state.unit ||
+                            !this.state.author ||
+                            !this.state.dateTime} 
+                        bsStyle="success"
+                        onClick={this.handleClearState}
+                    >Skapa Widget</Button>
                     
                 </form>
             </div>
