f6add169260deb247ae19e117f4b6fd8eee8439f
chadina <dina_1_96@hotmail.com>
2018-05-09 12:46:41 +0200

    footer
    delted footertext

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,5 +1,4 @@
 import './footer.css';
-import './footerText.css'
 import React, { Component } from 'react';
 
 
@@ -7,14 +6,26 @@ class Footer extends Component {
 
   constructor(props){
       super(props);
-
+      this.state = {toggle: true};
+      this.eventHandler = this.eventHandler.bind(this);
     }
+    eventHandler(event) {
+  this.setState((prevState) => ({
+      toggle: !prevState.toggle
+    })
+  );
+}
 
       render() {
           return (
 
               <div className="footer">
-                <div  className="footerText"> <p>footer</p> </div>
+                <div  className="footerText">
+                <p><a onClick={this.eventHandler}>
+                {this.state.toggle ? 'footer' : 'reset'}
+                </a>
+                </p>
+                </div>
               </div>
             )
           }
