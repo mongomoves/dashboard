e01758d2e2f727bdb8ca1385f9cc7b8bdd553a03
chadina <dina_1_96@hotmail.com>
2018-05-09 14:27:41 +0200

    footer
    created activity log

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,4 +1,5 @@
 import './footer.css';
+import BootstrapModal from '../Modal/BootstrapModal';
 import React, { Component } from 'react';
 
 
@@ -6,32 +7,44 @@ class Footer extends Component {
 
   constructor(props){
       super(props);
-      this.state = {toggle: true};
-      this.eventHandler = this.eventHandler.bind(this);
+          this.state = {
+            modals: {
+                showBootStrapModal: false
+            },
+          }
     }
-    eventHandler(event) {
-  this.setState((prevState) => ({
-      toggle: !prevState.toggle
-    })
-  );
-}
+
+    handleShowBootStrapModal = () => {
+        this.setState({modals: {showBootStrapModal: true}})
+    };
+
+    handleCloseBootStrapModal = () => {
+        this.setState({modals: {showBootStrapModal: false}})
+    };
+
+
 
       render() {
           return (
 
               <div className="footer">
+              <BootstrapModal
+                  title="Activity Log"
+                  show={this.state.modals.showBootStrapModal}
+                  close={this.handleCloseBootStrapModal}>
+                Datum: 180515<br />
+                Tid: 15.17<br />
+                Sven har lagt till cellen "Projekt"
+              </BootstrapModal>
                 <div  className="footerText">
-                <p><a onClick={this.eventHandler}>
-                {this.state.toggle ? 'footer' : 'reset'}
+                <p><a onClick={this.handleShowBootStrapModal}>
+                 footer
                 </a>
                 </p>
                 </div>
               </div>
             )
           }
-
-
-
 
         }
         export default Footer;
