e774e8b81d5aa078dc9424742d0f70335237bfec
LagAnton90 <lagerlof90@gmail.com>
2018-05-10 16:26:21 +0200

    Wrong URL or attribute handling

--- client/src/components/ValueComponent/ValueComponent.js
+++ client/src/components/ValueComponent/ValueComponent.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import './ValueComponent.css';
+import React, { Component } from "react";
+import "./ValueComponent.css";
 
 class ValueComponent extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            fetchResult: null,
             externalData: false,
-            fetchSuccess: true
-
+            fetchSuccess: false,
+            fetchResult: null
 
 
         }
@@ -19,6 +18,7 @@ class ValueComponent extends Component {
     //checking if external data is specified, and if true fetches it 
     componentDidMount() {
         if (this.props.dataSource && this.props.attribute) {
+            this.setState({ externalData: true })
             this.getData();
         }
     }
@@ -31,20 +31,15 @@ class ValueComponent extends Component {
         let dataURL = this.props.dataSource;
         let attribute = this.props.attribute;
 
-
-
         fetch(dataURL)
             .then(res => res.json())
             .then((out) => {
-                this.setState({ fetchResult: out[attribute], externalData: true });
 
+                if (out[attribute]) {
+                    this.setState({ fetchResult: out[attribute], fetchSuccess: true });
+                }
 
             })
-            .catch(err => {
-                this.setState({ fetchSuccess: false })
-
-            });
-
 
     }
 
@@ -55,7 +50,7 @@ class ValueComponent extends Component {
     render() {
 
         //if external data is used, it is rendered 
-        if (this.state.externalData) {
+        if (this.state.fetchSuccess) {
             return (
                 <div>
                     <span style={spanStyleNumber}>{this.state.fetchResult}</span>
@@ -64,6 +59,17 @@ class ValueComponent extends Component {
             )
 
         }
+        
+        //if something went wrong with external data
+        if (this.state.externalData && !this.state.fetchSuccess) {
+            return (
+                <div>
+                    <span style={spanStyleError}><b>Sorry!</b><br /> The entered URL or attribute was invalid.</span>
+                </div>
+             )
+        }
+
+
         //if no external data, props for number and unit is rendered
         return (
             <div>
@@ -88,6 +94,11 @@ const spanStyleNumber = {
 const spanStyleUnit = {
     fontSize: "150%",
     fontWeight: "bold",
+    color: "white"
+}
+
+const spanStyleError = {
+    fontSize: "100%",
     color: "white"
 }
 
