3c2e770b2aa8bf186d736ebdcce28142adc6797d
LagAnton90 <lagerlof90@gmail.com>
2018-05-11 15:02:58 +0200

    changes

--- client/src/components/ValueComponent/ValueComponent.js
+++ client/src/components/ValueComponent/ValueComponent.js
@@ -83,8 +83,8 @@ class ValueComponent extends Component {
         if (this.state.fetchSuccess) {
             return (
                 <div>
-                    <span style={{...spanStyleNumber, fontSize: `${this.props.width / 5}px`}}>{this.state.fetchContainer}</span>
-                    <span style={{...spanStyleUnit, fontSize: `${this.props.width / 9}px`}}>{this.props.unit}</span>
+                    <span style={{...spanStyleNumber, fontSize: `${this.props.width / 9}px`}}>{this.state.fetchContainer}</span>
+                    <span style={{...spanStyleUnit, fontSize: `${this.props.width / 10}px`}}>{this.props.unit}</span>
                 </div>
             )
 
