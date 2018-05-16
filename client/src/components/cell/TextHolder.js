ec2fbbf28c20d4338bfd1cf2baef298e523a452c
Viktor Ekström <mongomoves@gmail.com>
2018-05-16 11:55:13 +0200

    TextHolder version one

--- client/src/components/cell/TextHolder.js
+++ client/src/components/cell/TextHolder.js
@@ -11,29 +11,54 @@ class TextHolder extends Component {
         }
     }
 
+    /**
+     * Lifecycle method. Fetches if necessary or simple sets text from props.
+     */
     componentDidMount() {
-        this.fetchText();
-    }
-
-    fetchText = () => {
-        if(this.props.values.dataSource) {
-            fetch(this.props.values.dataSource)
-            .then(res => res.json())
-            .then((out) => {
-                const apiText = this.getValueByKey(out, this.props.values.attribute);
-                if(typeof apiText === 'string' || apiText instanceof String) {
-                    this.setState({text: apiText});
-                } else if (Array.isArray(apiText)) {
-                    this.setState({isArray: true, text: apiText});
-                } else {
-                    this.setState({text: 'Felaktigt attribut, ingen data hämtad'});
-                }
-            });
-        } else if (this.props.values.textInput) {
+        if(this.props.values.dataSource && this.props.values.attribute) {
+            this.fetchText(this.props.values.dataSource, this.props.values.attribute);
+        } else {
             this.setState({text: this.props.values.textInput});
         }
     }
 
+    /**
+     * Lifecycle method. Makes sure the component updates properly when
+     * the user switches between using free text and api requests.
+     */
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.values.textInput && (prevState.text !== this.props.values.textInput)) {
+            this.setState({text: this.props.values.textInput, isArray: false});
+        } else if(prevProps.values.dataSource !== this.props.values.dataSource || 
+            prevProps.values.attribute !== this.props.values.attribute) {
+                this.fetchText(this.props.values.dataSource, this.props.values.attribute);
+            }
+    }
+
+    /**
+     * Fetches some text from the provided URL with the provided key (attribute).
+     * Sets isArray to true if an array is returned, so that it can be displayed.
+     * An incorrect fetch will display a fail message.
+     */
+    fetchText = (url, attribute) => {
+        fetch(url)
+        .then(res => res.json())
+        .then((out) => {
+            const apiText = this.getValueByKey(out, attribute);
+            if(typeof apiText === 'string' || apiText instanceof String) {
+                this.setState({text: apiText, isArray: false});
+            } else if (Array.isArray(apiText)) {
+                this.setState({isArray: true, text: apiText});
+            } else {
+                this.setState({text: 'Felaktigt attribut, ingen data hämtad'});
+            }
+        });
+    }
+
+    /**
+     * Returns the value for the specific key, even if value 
+     * is nested.
+     */
     getValueByKey = (object, key) => {
         var stack = [object];
         var current, index, value;
@@ -51,19 +76,33 @@ class TextHolder extends Component {
         }
     }
 
+    /**
+     * Returns an array of elements with a div for each
+     * entry in the array to display.
+     */
     printArray = (array) => {
         let newArray = array.map(el => {
-          return (
-                        <span style={{...spanStyleText, fontSize: `${this.calcFont()}%`}}>
-                            {JSON.stringify(el)}
-                        </span>
+            let newEl;
+            if(el instanceof Object) {
+                newEl = JSON.stringify(el);
+            } else {
+                newEl = String(el);
+            }
+        return (
+            <div key={el} style={{...spanStyleText, fontSize: `${this.calcFont()}%`}}>
+                {newEl}
+            </div>
             )
         });
         return newArray;
     }
 
+    /**
+     * Calculates the font size for the TextHolder, so that it is
+     * neither too big nor too small.
+     * Returns the size in percentage.
+     */
     calcFont = () => {
-        // let size = 0.5 * (this.props.width + 16);
         let size = this.props.width / 2;
         if(size < 200) {
             return 200;
@@ -77,8 +116,7 @@ class TextHolder extends Component {
             content = this.printArray(this.state.text);
         } else {
             content = (
-                //<span style={{...spanStyleText, fontSize: `${this.props.width / 2}%`}}>{this.state.text}</span>
-                <span style={{...spanStyleText, fontSize: `${this.calcFont()}%`}}>{this.props.values.textInput}</span>
+                <span style={{...spanStyleText, fontSize: `${this.calcFont()}%`}}>{this.state.text}</span>
             )
         }
         return (
@@ -91,6 +129,7 @@ class TextHolder extends Component {
 
 export default TextHolder;
 
+
 const spanStyleText = {
     fontWeight: "bold",
     display: "inlineBlock",
@@ -98,10 +137,11 @@ const spanStyleText = {
     color: "orange"
 };
 
+//Style for the div displaying the text.
 const divStyle={
     overflowX: 'hidden',
     textOverflow: 'ellipsis',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
+    whiteSpace: 'pre-line',
+    display: 'block',
+    justifyContent: 'center'
 };
