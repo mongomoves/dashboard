27500d1d5d7ae537a0d42fb44ce1cc86468eee56
chadina <dina_1_96@hotmail.com>
2018-05-18 21:30:52 +0200

    fetch data

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -8,6 +8,7 @@ class Footer extends Component {
   constructor(props){
       super(props);
           this.state = {
+        //    logg: [],
             modals: {
                 showBootStrapModal: false
             },
@@ -24,33 +25,33 @@ class Footer extends Component {
     };
 
 componentWillMount() {
-
+  console.log("componentWillMount");
   this.getData();
 
  }
 
 getData = () =>{
-  fetch('http://localhost:3001/api/widgets')
+  fetch('http://192.168.99.100:3001/api/log')
    .then(results => {
      return results.json();
-   }).then(data => {
-     let logg = data.widgets;
-     console.log(logg);
-     // this.setState(prevState => ({
-     //   cells: [...prevState.cells, cells]
-     // }))
- this.setState({ logg: logg });
-   })
+   }).then(function(data){
+   console.log(data);
+  // let logg = data.log;
+
+  // this.setState({ logg: data.results });
+   }.bind(this))
+   .catch(err => {
+      console.log("ERROR: " + err);
+    })
+
 
 
 
 }
 
       render() {
-        console.log("Detta är returnerad data: " + this.state.logg);
 
           return (
-
 
             <div className="footer">
               <BootstrapModal
@@ -61,6 +62,7 @@ getData = () =>{
                   Tid: 15.17<br />
                   {this.state.logg}
                   Sven har lagt till cellen "Projekt"
+
                   <div className="logg">
 
                   </div>
