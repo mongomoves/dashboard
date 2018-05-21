e9d2c5105b02ea34c4853992a643a643b9f05d32
Myviktoria <Myhaegg@hotmail.com>
2018-05-21 12:48:13 +0200

    added text

--- client/src/components/HowToPage/ContentComponents/StegforStegComponent.js
+++ client/src/components/HowToPage/ContentComponents/StegforStegComponent.js
@@ -35,9 +35,10 @@ class StepByStep extends Component {
             <div>
                 <li className="liBold">3. Steg för steg</li>
                     <p>
-                        Här ges ett flertal exempel på hur man kommer igång med sin Dashboard samt håller den uppdaterad. 
+                        Här ges ett flertal exempel på hur man kommer igång med sin Dashboard samt håller den uppdaterad.
+                    </p>  
                         <li><a href="#" onClick={this.togglePublishHandler}>Skapa en widget med typen värde. <Glyphicon glyph="menu-down" /></a></li>
-                        <img src="https://i.imgur.com/tbtPqDK.png" alt="Create Widget"/>
+                        <img src="https://i.imgur.com/tbtPqDK.png" alt="Skapa Widget"/>
                         <ul>
                             <li>3.1 Välj Widget-typ. Här kan man välja mellan tre olika typer. Värde, diagram och text.</li>
                             <li>3.2 Ange titel (Titeln är även parametern som anges när man vill ladda widgets)</li>
@@ -49,9 +50,11 @@ class StepByStep extends Component {
                             <li>3.8 Sista steget när man skapar en widget är att kryssa i om man vill få sin widget <u>publicerad</u>.</li>
                         </ul>
                         <li><a href="#" onClick={this.togglePublishHandler}>Redigera widget <Glyphicon glyph="menu-down" /></a></li>
+                        <img src="" alt="Redigera widget"/>
+                        <ul>
+                            <li> Hejsan hoppsan</li>
+                        </ul>
                         <li><a href="#" onClick={this.togglePublishHandler}>Publicera widget <Glyphicon glyph="menu-down" /></a></li>
-                    
-                    </p>
                     <p>
                         För att kunna skapa en widget börjar man med att trycka på navigationsfältets kugghjul till höger.
                     </p>
