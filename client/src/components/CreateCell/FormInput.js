4be32a68e4263449ac3e6ee0406d3e2a909fea16
Viktor Ekström <mongomoves@gmail.com>
2018-05-21 11:04:46 +0200

    FormInput component, CreateCellFormat refactor

---
+++ client/src/components/CreateCell/FormInput.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {FormGroup, FormControl, ControlLabel, OverlayTrigger, Tooltip} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+class FormInput extends React.Component {
+    render() {
+        return (
+            <FormGroup>
+                <ControlLabel>{this.props.title}</ControlLabel>
+                <OverlayTrigger placement="top" overlay={<Tooltip id="tooltip-number">{this.props.tooltip}</Tooltip>}>
+                    <FormControl
+                        type={this.props.type}
+                        onChange={this.props.onChange}/>
+                </OverlayTrigger>
+            </FormGroup>
+        )
+    }
+}
+
+export default FormInput;
+
+FormInput.propTypes = {
+    title: PropTypes.string.isRequired,
+    tooltip: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired
+}
