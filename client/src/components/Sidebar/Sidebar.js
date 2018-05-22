8d6efaa893a97ec7919e26331b9ce24c27f23f88
Myviktoria <Myhaegg@hotmail.com>
2018-05-22 14:15:40 +0200

    Fixed glyphicon collapse

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -9,17 +9,35 @@ class Sidebar extends Component {
         super(props);
         this.state = {
             Sidebar: false,
+            CollapseKoncept: false,
+            CollapseWidget: false,
+            CollapseStep: false,
             cog: <Glyphicon glyph="menu-down" />
         }
+
     }
 
     render() {
         return (
             <div>
                 <div className="vertical-list">
-                    <MenuItem eventKey={4.1} onClick={this.props.clickedKoncept} className="button">Koncept<Glyphicon glyph={this.state.collapse ? "chevron-left" : "chevron-right"}/></MenuItem>
-                    <MenuItem eventKey={4.2} onClick={this.props.clickedWidgetEx} className="button">Widget</MenuItem>
-                    <MenuItem eventKey={4.3} onClick={this.props.clickedStepByStep} className="button">Steg för steg</MenuItem>
+                    <MenuItem eventKey={4.1} onClick={() => {
+                            this.setState({CollapseKoncept: !this.state.CollapseKoncept}); 
+                            this.props.clickedKoncept()}} className="button">Koncept
+                        <Glyphicon glyph={this.state.CollapseKoncept ? "chevron-right" : "chevron-left"}/>
+                    </MenuItem>
+
+                    <MenuItem eventKey={4.2} onClick={() => {
+                            this.setState({CollapseWidget: !this.state.CollapseWidget}); 
+                            this.props.clickedWidgetEx()}} className="button">Widget
+                        <Glyphicon glyph={this.state.CollapseWidget ? "chevron-left" : "chevron-right"}/>
+                    </MenuItem>
+
+                    <MenuItem eventKey={4.3} onClick={() => {
+                            this.setState({CollapseStep: !this.state.CollapseStep}); 
+                            this.props.clickedStepByStep()}} className="button">Steg för steg
+                        <Glyphicon glyph={this.state.CollapseStep ? "chevron-left" : "chevron-right"}/>
+                    </MenuItem>
                 </div>
             </div>
         );
