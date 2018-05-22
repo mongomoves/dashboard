dc70fd56a2e519797c89fb736229edf76b89424b
ViktorOlsson <vike_olsson@hotmail.com>
2018-05-22 21:12:03 +0200

    La in en route som fångar alla felaktiga routes.

---
+++ client/src/components/RenderPath/catchUrl.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class CatchUrl extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Sorry, no page with this url exists...</h2>
+            </div>
+        );
+    }
+}
+
+export default CatchUrl;
