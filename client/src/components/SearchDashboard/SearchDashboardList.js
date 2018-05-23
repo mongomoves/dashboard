793d635b1b292d8918e020747a5cb0331aad9098
RomanKuroptev <roman.kuroptev@protonmail.com>
2018-05-23 11:42:11 +0200

    Removed unnesecary logic

--- client/src/components/SearchDashboard/SearchDashboardList.js
+++ client/src/components/SearchDashboard/SearchDashboardList.js
@@ -10,14 +10,10 @@ class SearchDashboardList extends React.Component {
 
     createListItems = () => {
         const addDashboard = this.props.addDashboard;
-        const query = this.props.query;
         return _.map(this.props.dashboards, function (dashboard) {
-            if(( dashboard.title.indexOf( query ) > -1 ) || ( dashboard.description.indexOf(query) > -1 )||( dashboard.creator.indexOf( query ) > -1 )) {
                 return (
-
                     <SearchDashboardListItem key={dashboard._id} addDashboard={addDashboard} content={dashboard}/>
                 );
-            }
         });
     };
 
